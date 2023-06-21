b4b9f2d580908a876ec9aee57dc7d53289de7c40
thomas <130037065+thomas9291@users.noreply.github.com>
2023-06-21 16:47:12 +0200

    layout not responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,24 +7,86 @@ import NavBar from "../../component/NavBar/NavBar";
 import CartDetail from "component/CartDetail/CartDetail";
 import MyMap from "../../component/MyMap/MyMap";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { EffectCoverflow, Pagination } from "swiper";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Component() {
   const { data: session } = useSession();
-  const { data } = useSWR("/api/places", { fallbackData: [] });
-  console.log("data:", data);
+  const { data: placesList } = useSWR("/api/places", { fallbackData: [] });
+  console.log("placesList:", placesList);
 
   console.log(session);
   if (session) {
     return (
       <>
         <NavBar onClick={() => signOut()} />
-        <div className="d-flex flex-column align-items-center">
+        <div className="d-flex flex-column align-items-center ">
           <div>
             <MyMap />
           </div>
-          <div>
-            <CartDetail />
+          <div
+            style={{ width: "100%" }}
+            className="border border-3 border-white"
+          >
+            <Swiper
+              effect={"coverflow"}
+              grabCursor={true}
+              centeredSlides={true}
+              slidesPerView={"auto"}
+              coverflowEffect={{
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: true,
+              }}
+              pagination={true}
+              modules={[EffectCoverflow, Pagination]}
+              className="mySwiper"
+            >
+              {placesList.map(
+                ({
+                  name,
+                  address,
+                  latitude,
+                  longitude,
+                  favorite,
+                  description,
+                  grill,
+                  beatch,
+                  camping,
+                  shore,
+                  boat,
+                  _id,
+                }) => {
+                  return (
+                    <SwiperSlide key={_id}>
+                      <CartDetail
+                        name={name}
+                        image={
+                          "https://images.unsplash.com/photo-1618570395080-674aff5b5046?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGFjfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+                        }
+                        address={address}
+                        latitude={latitude}
+                        longitude={longitude}
+                        favorite={favorite}
+                        description={description}
+                        grill={grill}
+                        beatch={beatch}
+                        camping={camping}
+                        shore={shore}
+                        boat={boat}
+                      />
+                    </SwiperSlide>
+                  );
+                }
+              )}
+            </Swiper>
           </div>
         </div>
       </>
